e1ee5da60e2db647da410357473643f338638106
lysszapata <147885594+lysszapata@users.noreply.github.com>
2023-10-18 14:33:23 -0700

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,16 @@
+let exampleObject = {
+    id: 1,
+    name: "Alyssa Zapata"
+}
+
+console.log(exampleObject);
+console.log("My name is: ",exampleObject.name);
+
+function doSomething() {
+    let functionVar = 10;
+    for(let i =0; i <10; i++) {
+        console.log(i);
+    }
+}
+
+doSomething();
